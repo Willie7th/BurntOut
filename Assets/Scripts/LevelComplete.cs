626525c9ff7befcdbce1a8174d1df72070fe8bc3
Stuart Heath <stuartheath02@gmail.com>
2024-10-13 19:11:21 +0200

    I COOKED MORE

---
+++ Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelComplete : MonoBehaviour
+{
+    
+    private List<GameObject> emberPrefabs = new List<GameObject>();
+
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            Debug.Log(child);
+            emberPrefabs.Add(child.gameObject);
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        foreach (GameObject ember in emberPrefabs)
+        {
+            ember.SetActive(true);
+        }
+        
+    }
+}
